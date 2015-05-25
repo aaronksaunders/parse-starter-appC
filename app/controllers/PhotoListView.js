91628701907b504462dc46e0df859aa0f73eafca
aaronksaunders <aaron@clearlyinnovative.com>
2015-05-25 17:10:36 -0400

    android image upload and menu item integrated into sample

--- app/controllers/PhotoListView.js
+++ app/controllers/PhotoListView.js
@@ -34,28 +34,28 @@ Ti.API.info('Loaded PhotoListView Controller');
  * screen when data is done loading
  */
 $.loadImages = function loadImages(_callback) {
-    Ti.API.info('PhotoListView Controller: loadImages');
+	Ti.API.info('PhotoListView Controller: loadImages');
 
-    utils.showIndicator();
+	utils.showIndicator();
 
-    // call ACS to get current List of photos
-    parseService.getObjects("ImageInfo", {}).then(function(_data) {
-        var _photos = _data.response.results;
-        Ti.API.info("_photos: " + JSON.stringify(_photos, null, 2));
+	// call ACS to get current List of photos
+	parseService.getObjects("ImageInfo", {}).then(function(_data) {
+		var _photos = _data.response.results;
+		Ti.API.info("_photos: " + JSON.stringify(_photos, null, 2));
 
-        // add photos to UI
-        addPhotosToListView(_photos);
+		// add photos to UI
+		addPhotosToListView(_photos);
 
-    }).finally(function() {
-        utils.hideIndicator();
+	}).finally(function() {
+		utils.hideIndicator();
 
-        // indicate we are finished loading, used by
-        // refreshed
-        _callback && _callback();
+		// indicate we are finished loading, used by
+		// refreshed
+		_callback && _callback();
 
-    }, function(_error) {
-        alert('Error:\n' + ((_error.error && _error.message) || JSON.stringify(_error)));
-    });
+	}, function(_error) {
+		alert('Error:\n' + ((_error.error && _error.message) || JSON.stringify(_error)));
+	});
 };
 
 /**
@@ -63,34 +63,34 @@ $.loadImages = function loadImages(_callback) {
  */
 function addPhotosToListView(_photos) {
 
-    // empty the list out
-    $.listViewSection.deleteItemsAt(0, $.listViewSection.items.length);
+	// empty the list out
+	$.listViewSection.deleteItemsAt(0, $.listViewSection.items.length);
 
-    for (var i = _photos.length - 1; i >= 0; i--) {
+	for (var i = _photos.length - 1; i >= 0; i--) {
 
-        // _photos[i].created_at - format the date using moment library
-        var convertedDate = moment(_photos[i].createdAt).format('MMMM Do YYYY, h:mm:ss a');
-        var dateAndName = _photos[i].caption + " - " + convertedDate;
+		// _photos[i].created_at - format the date using moment library
+		var convertedDate = moment(_photos[i].createdAt).format('MMMM Do YYYY, h:mm:ss a');
+		var dateAndName = _photos[i].caption + " - " + convertedDate;
 
-        var listItem = {
-            properties : {
-                photoObject : _photos[i]
-            },
-            fileName : {
-                text : _photos[i].picture.name
-            },
-            dateCreated : {
-                //text :
-                text : dateAndName
-            },
-            template : 'listViewTemplate',
-            thumbImage : {
-                image : _photos[i].picture.url
-            }
-        };
+		var listItem = {
+			properties : {
+				photoObject : _photos[i]
+			},
+			fileName : {
+				text : _photos[i].picture.name
+			},
+			dateCreated : {
+				//text :
+				text : dateAndName
+			},
+			template : 'listViewTemplate',
+			thumbImage : {
+				image : _photos[i].picture.url
+			}
+		};
 
-        $.listViewSection.appendItems([listItem]);
-    }
+		$.listViewSection.appendItems([listItem]);
+	}
 }
 
 /**
@@ -99,69 +99,69 @@ function addPhotosToListView(_photos) {
  * @param {Object} _event
  */
 function refreshData(_event) {
-    $.loadImages(function() {
-        _event.hide();
-    });
+	$.loadImages(function() {
+		_event.hide();
+	});
 }
 
 function listItemClicked(_event) {
 
-    var currentItem = $.listViewSection.getItemAt(_event.itemIndex);
-    var selectedObject = currentItem.properties.photoObject;
+	var currentItem = $.listViewSection.getItemAt(_event.itemIndex);
+	var selectedObject = currentItem.properties.photoObject;
 
-    // log for debugging purposes and convert object to
-    // string that is readable
-    console.log("selectedObject " + JSON.stringify(selectedObject, null, 2));
+	// log for debugging purposes and convert object to
+	// string that is readable
+	console.log("selectedObject " + JSON.stringify(selectedObject, null, 2));
 
-    // create the new controller and pass in the
-    // model object as an argument 'item'
-    var ctrl = Alloy.createController('PhotoDetail', {
-        'item' : selectedObject,
-        'photoListTab' : $.photoListTab
-    });
+	// create the new controller and pass in the
+	// model object as an argument 'item'
+	var ctrl = Alloy.createController('PhotoDetail', {
+		'item' : selectedObject,
+		'photoListTab' : $.photoListTab
+	});
 
-    setTimeout(function() {
-        $.photoListTab.open(ctrl.detailWindow);
-    }, 200);
+	setTimeout(function() {
+		$.photoListTab.open(ctrl.detailWindow);
+	}, 200);
 }
 
 /**
  * http://docs.appcelerator.com/titanium/3.0/#!/guide/Camera_and_Photo_Gallery_APIs
  */
-function addPhoto() {
+$.addPhoto = function addPhoto() {
 
-    Titanium.Media.openPhotoGallery({
-        //Titanium.Media.showCamera({
-        success : function(event) {
-            // called when media returned from the camera
-            Ti.API.debug('Our type was: ' + event.mediaType);
-            if (event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
+	Titanium.Media.openPhotoGallery({
+		//Titanium.Media.showCamera({
+		success : function(event) {
+			// called when media returned from the camera
+			Ti.API.debug('Our type was: ' + event.mediaType);
+			if (event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
 
-                // now save the photo with no location
-                savePhoto(event.media, {});
+				// now save the photo with no location
+				savePhoto(event.media, {});
 
-            } else {
-                alert("got the wrong type back =" + event.mediaType);
-            }
-        },
-        cancel : function() {
-            // called when user cancels taking a picture
-        },
-        error : function(error) {
-            // called when there's an error
-            if (error.code == Titanium.Media.NO_CAMERA) {
-                alert('Please run this test on device');
-            } else {
-                alert('Unexpected error: ' + error.code);
-            }
-        },
-        saveToPhotoGallery : true,
-        // allowEditing and mediaTypes are iOS-only settings
-        allowEditing : true,
-        // only photo, no videos in this sample
-        mediaTypes : [Ti.Media.MEDIA_TYPE_PHOTO]
-    });
-}
+			} else {
+				alert("got the wrong type back =" + event.mediaType);
+			}
+		},
+		cancel : function() {
+			// called when user cancels taking a picture
+		},
+		error : function(error) {
+			// called when there's an error
+			if (error.code == Titanium.Media.NO_CAMERA) {
+				alert('Please run this test on device');
+			} else {
+				alert('Unexpected error: ' + error.code);
+			}
+		},
+		saveToPhotoGallery : true,
+		// allowEditing and mediaTypes are iOS-only settings
+		allowEditing : true,
+		// only photo, no videos in this sample
+		mediaTypes : [Ti.Media.MEDIA_TYPE_PHOTO]
+	});
+};
 
 /**
  *
@@ -170,48 +170,44 @@ function addPhoto() {
  */
 function savePhoto(_imageData, _locationInformation) {
 
-    //utils.showindicator();
+	//utils.showindicator();
 
-    // compress image for better uploading
+	// compress image for better uploading
 
-    var imageCompressed;
+	var imageCompressed;
 
-    if (OS_ANDROID || _imageData.width > 700) {
-        var w,
-            h;
-        w = _imageData.width * .50;
-        h = _imageData.height * .50;
-        imageCompressed = ImageFactory.imageAsResized(_imageData, {
-            width : w,
-            height : h
-        });
-    } else {
-        // we do not need to compress here
-        imageCompressed = _imageData;
-    }
+	if (OS_ANDROID || _imageData.width > 700) {
+		var w,
+		    h;
+		w = _imageData.width * .50;
+		h = _imageData.height * .50;
+		imageCompressed = ImageFactory.imageAsResized(_imageData, {
+			width : w,
+			height : h
+		});
+	} else {
+		// we do not need to compress here
+		imageCompressed = _imageData;
+	}
 
-    parseService.uploadFile("image/jpeg", Ti.Platform.createUUID() +".jpeg", imageCompressed).then(function(_results) {
-        return parseService.createObject('ImageInfo', {
-            "caption" : _results.response.name,
-            "picture" : {
-                "name" : _results.response.name,
-                "__type" : "File"
-            }
-        }).then(function(_results2) {
-            console.log("FileHelper Object: " + JSON.stringify(_results2));
+	parseService.uploadFile("image/jpeg", Ti.Platform.createUUID() + ".jpeg", imageCompressed).then(function(_results) {
+		return parseService.createObject('ImageInfo', {
+			"caption" : _results.response.name,
+			"picture" : {
+				"name" : _results.response.name,
+				"__type" : "File"
+			}
+		}).then(function(_results2) {
+			console.log("FileHelper Object: " + JSON.stringify(_results2));
 
-            return $.photoListView.loadImages();
-        }, function(_error) {
-            console.log("ERROR: " + JSON.stringify(_error));
-        });
-    });
+			return $.photoListView.loadImages();
+		}, function(_error) {
+			console.log("ERROR: " + JSON.stringify(_error));
+		});
+	});
 
 }
 
-// MenuItem Event, cannot be set until the menu is drawn
-OS_ANDROID && ($.photoListWindow.activity.onPrepareOptionsMenu = function(e) {
-    e.menu.findItem(0).addEventListener('click', addPhoto);
-});
 
 // Button Event
 OS_IOS && $.addPhotoBtn.addEventListener('click', addPhoto);
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,36 @@
-'use strict';
+Alloy.Globals.tabGroup = $.index;
 
-var sampleTableData = [];
+function doWindowOpen(evt) {
+
+	var activity = evt.source.getActivity();
+
+	activity.onCreateOptionsMenu = function(e) {
+		var item, menu;
+		menu = e.menu;
+		menu.clear();
+		
+		switch(Alloy.Globals.currentTab){
+			// case photoList
+			case 1:
+				item2 = e.menu.add({
+					title : "Add",
+					showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS,
+				});
+				item2.addEventListener("click", function(e) {					
+					$.photoListView.addPhoto();
+				});
+				
+				break;
+		}
+	};
+
+	Alloy.Globals.tabGroup.addEventListener("focus", function(evt) {
+		if (typeof evt.index !== "undefined"){
+			activity.invalidateOptionsMenu();
+			Alloy.Globals.currentTab=evt.index;	
+		} 
+	});
+}
 
 /**
  * called when user clicks on listView. The _event will provide
@@ -9,19 +39,17 @@ var sampleTableData = [];
  * @param {Object} _event
  */
 function listItemClicked(_event) {
-    // display to console log the _event object for debugging
-    Ti.API.debug(JSON.stringify(_event, null, 2));
 
-    // get data using index provided, the items are in the section
-    // so we use the index against the section, not the listView
-    var currentItem = $.listSection.getItemAt(_event.itemIndex);
+	// get data using index provided, the items are in the section
+	// so we use the index against the section, not the listView
+	var currentItem = $.listSection.getItemAt(_event.itemIndex);
 
-    // we save the data as a property so that is how we access
-    // the data, we do not need the data array in this case
-    var sessionInfo = currentItem.properties.data;
+	// we save the data as a property so that is how we access
+	// the data, we do not need the data array in this case
+	var sessionInfo = currentItem.properties.data;
 
-    // now display the data
-    alert("clicked on " + JSON.stringify(sessionInfo, null, 2));
+	// now display the data
+	alert("clicked on " + JSON.stringify(sessionInfo, null, 2));
 }
 
 // lets get some real data!!
@@ -33,69 +61,69 @@ Alloy.Globals.parseService.init();
 var parseService = Alloy.Globals.parseService;
 
 function saveSessionClicked() {
-    var queryResults;
+	var queryResults;
 
-    alert("WIP");
-    return;
+	alert("WIP");
+	return;
 
-    parseService.createObject('TutorSession', {
-        sessionName : $.sessionName.value,
-        sessionLocation : $.sessionLocation.value
-    }).then(function(_queryResult) {
-        console.log("Success Happened: " + JSON.stringify(_queryResult));
-        queryResults = _queryResult;
-        return parseService.getObjects('TutorSession');
-    }).then(function(_sessions) {
-        updateList(_sessions.response.results);
-    }, function(_error) {
-        console.log("Some Error Happened: " + JSON.stringify(_error));
-    });
+	parseService.createObject('TutorSession', {
+		sessionName : $.sessionName.value,
+		sessionLocation : $.sessionLocation.value
+	}).then(function(_queryResult) {
+		console.log("Success Happened: " + JSON.stringify(_queryResult));
+		queryResults = _queryResult;
+		return parseService.getObjects('TutorSession');
+	}).then(function(_sessions) {
+		updateList(_sessions.response.results);
+	}, function(_error) {
+		console.log("Some Error Happened: " + JSON.stringify(_error));
+	});
 }
 
 function updateList(_data) {
-    var items = _.map(_data, function(element) {
+	var items = _.map(_data, function(element) {
 
-        return {
-            properties : {
-                data : element // save all attributes
-            },
-            // bind the labels using the bindId
-            name : {
-                text : element.tutor.email
-            },
-            location : {
-                text : element.place.Location + "-" + element.place.Name
-            }
-        };
-    });
-    // add the items to the section in the ListView
-    $.listSection.setItems(items);
+		return {
+			properties : {
+				data : element // save all attributes
+			},
+			// bind the labels using the bindId
+			name : {
+				text : element.tutor.email
+			},
+			location : {
+				text : element.place.Location + "-" + element.place.Name
+			}
+		};
+	});
+	// add the items to the section in the ListView
+	$.listSection.setItems(items);
 
 }
 
 function whereQueryExample() {
 
-    // find all tutoring sessions that are being done by
-    // the tutors with the specified ids
-    var whereQueryStr = {
-        "tutor" : {
-            "$inQuery" : {
-                "where" : {
-                    "objectId" : {
-                        "$in" : ["SCV3V0GqRr", "9uKbg0Hzeb"]
-                    }
-                },
-                "className" : "_User"
-            }
-        }
-    };
+	// find all tutoring sessions that are being done by
+	// the tutors with the specified ids
+	var whereQueryStr = {
+		"tutor" : {
+			"$inQuery" : {
+				"where" : {
+					"objectId" : {
+						"$in" : ["SCV3V0GqRr", "9uKbg0Hzeb"]
+					}
+				},
+				"className" : "_User"
+			}
+		}
+	};
 
-    return parseService.getObjects('TutorSession', {
-        "urlparams" : {
-            "include" : "user,tutor,place",
-            "where" : whereQueryStr
-        }
-    });
+	return parseService.getObjects('TutorSession', {
+		"urlparams" : {
+			"include" : "user,tutor,place",
+			"where" : whereQueryStr
+		}
+	});
 }
 
 /**
@@ -103,24 +131,24 @@ function whereQueryExample() {
  * of the rest service
  */
 parseService.loginUser("aaronsaunders", "password").then(function(_result) {
-    console.log("Success: " + JSON.stringify(_result, null, 2));
-    //return parseService.getObjects('TutorSession');
+	console.log("Success: " + JSON.stringify(_result, null, 2));
+	//return parseService.getObjects('TutorSession');
 
-    return whereQueryExample();
+	return whereQueryExample();
 
 }).then(function(_sessions) {
-    console.log("getTutorSessions: " + JSON.stringify(_sessions.response, null, 2));
+	console.log("getTutorSessions: " + JSON.stringify(_sessions.response, null, 2));
 
-    updateList(_sessions.response.results);
+	updateList(_sessions.response.results);
 
 }, function(_error) {
-    Ti.API.error('ERROR: ' + JSON.stringify(_error, null, 2));
+	Ti.API.error('ERROR: ' + JSON.stringify(_error, null, 2));
 });
 
 Ti.App.addEventListener("parse.push.recieved", function(_event) {
-    //if clicks push tray, it will take to alert list page
+	//if clicks push tray, it will take to alert list page
 
-    OS_IOS && Titanium.UI.iPhone.setAppBadge(0);
+	OS_IOS && Titanium.UI.iPhone.setAppBadge(0);
 });
 
 // open the main view of index.js, which is the tab
