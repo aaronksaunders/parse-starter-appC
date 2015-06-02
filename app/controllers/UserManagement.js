957598193933c3f4520854ce3298b9fb10b2e6c1
Aaron Saunders <aaron@clearlyinnovative.com>
2015-06-01 23:42:09 -0400

    added code for logging in and account creation in UserManagement Controller

---
+++ app/controllers/UserManagement.js
@@ -0,0 +1,109 @@
+var args = arguments[0] || {};
+// function to call after login or user created
+var callback = args.callback;
+/**
+ *
+ */
+function handleLoginClick(_event) {
+
+    Ti.API.debug('clicked: ' + _event.source.id);
+
+    var aUser = Alloy.createModel('User');
+    aUser.login($.login_email.value, $.login_password.value).then(function(_model) {
+        // Do stuff after successful login.
+        Alloy.Globals.loggedIn = true;
+        Alloy.Globals.CURRENT_USER = _model;
+        callback && callback(_model);
+    }, function(_error) {
+        var errorMsg = JSON.stringify(_error);
+        alert(_error.message);
+        Ti.API.error('Error: ' + errorMsg);
+    });
+}
+
+/**
+ *
+ */
+function handleShowAcctClick(_event) {
+
+    Ti.API.debug('clicked: ' + _event.source.id);
+
+    var animation = require('alloy/animation');
+
+    // when move the create account screen into view
+    var moveToTop = Ti.UI.createAnimation({
+        top : '0dp',
+        duration : 1
+    });
+    $.createAcctView.animate(moveToTop, function() {
+
+        // now cross fade
+        animation.crossFade($.loginAcctView, $.createAcctView, 500, function() {
+            // when done animating, move the view off screen
+            var moveToBottom = Ti.UI.createAnimation({
+                top : '500dp',
+                duration : 1
+            });
+            $.loginAcctView.animate(moveToBottom);
+        });
+    });
+}
+
+/**
+ *
+ */
+function handleCreateAccountClick() {
+    if ($.acct_password.value !== $.acct_password_confirmation.value) {
+        alert("Please re-enter information");
+        return;
+    }
+    var params = {
+        first_name : $.acct_fname.value,
+        last_name : $.acct_lname.value,
+        username : $.acct_email.value,
+        email : $.acct_email.value,
+        password : $.acct_password.value,
+        password_confirmation : $.acct_password_confirmation.value,
+    };
+    
+    var user = Alloy.createModel('User');
+    user.createAccount(params).then(function(_model) {
+        // Do stuff after successful creation of user.
+        Alloy.Globals.loggedIn = true;
+        Alloy.Globals.CURRENT_USER = _model;
+        callback && callback(_model);
+    }, function(_error) {
+        var errorMsg = JSON.stringify(_error);
+        alert(_error.message);
+        Ti.API.error('Error: ' + errorMsg);
+    });
+};
+
+/**
+ *
+ * @param {Object} _event
+ */
+function handleShowLoginClick(_event) {
+
+    Ti.API.debug('clicked: ' + _event.source.id);
+
+    var animation = require('alloy/animation');
+
+    // when move the login screen into view
+    var moveToTop = Ti.UI.createAnimation({
+        top : '0dp',
+        duration : 1
+    });
+    $.loginAcctView.animate(moveToTop, function() {
+
+        // now cross fade
+        animation.crossFade($.createAcctView, $.loginAcctView, 500, function() {
+            // when done animating, move the view off screen
+            var moveToBottom = Ti.UI.createAnimation({
+                top : '500dp',
+                duration : 1
+            });
+            $.createAcctView.animate(moveToBottom);
+        });
+    });
+}
