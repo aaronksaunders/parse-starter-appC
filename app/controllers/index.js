08371befaadf9c9edc52939926f20c2573a605cc
Aaron Saunders <aaron@clearlyinnovative.com>
2015-06-01 23:26:53 -0400

    added code to support logging in and out

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,40 +1,41 @@
 Alloy.Globals.tabGroup = $.index;
 
 function doWindowOpen(evt) {
-    
+
     if (OS_IOS) {
         return;
     }
 
-	var activity = evt.source.getActivity();
+    var activity = evt.source.getActivity();
 
-	activity.onCreateOptionsMenu = function(e) {
-		var item, menu;
-		menu = e.menu;
-		menu.clear();
-		
-		switch(Alloy.Globals.currentTab){
-			// case photoList
-			case 1:
-				item2 = e.menu.add({
-					title : "Add",
-					showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS,
-					icon: Ti.Android.R.drawable.ic_menu_camera
-				});
-				item2.addEventListener("click", function(e) {					
-					$.photoListView.addPhoto();
-				});
-				
-				break;
-		}
-	};
+    activity.onCreateOptionsMenu = function(e) {
+        var item,
+            menu;
+        menu = e.menu;
+        menu.clear();
 
-	Alloy.Globals.tabGroup.addEventListener("focus", function(evt) {
-		if (typeof evt.index !== "undefined"){
-			activity.invalidateOptionsMenu();
-			Alloy.Globals.currentTab=evt.index;	
-		} 
-	});
+        switch(Alloy.Globals.currentTab) {
+        // case photoList
+        case 1:
+            item2 = e.menu.add({
+                title : "Add",
+                showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS,
+                icon : Ti.Android.R.drawable.ic_menu_camera
+            });
+            item2.addEventListener("click", function(e) {
+                $.photoListView.addPhoto();
+            });
+
+            break;
+        }
+    };
+
+    Alloy.Globals.tabGroup.addEventListener("focus", function(evt) {
+        if ( typeof evt.index !== "undefined") {
+            activity.invalidateOptionsMenu();
+            Alloy.Globals.currentTab = evt.index;
+        }
+    });
 }
 
 /**
@@ -45,16 +46,16 @@ function doWindowOpen(evt) {
  */
 function listItemClicked(_event) {
 
-	// get data using index provided, the items are in the section
-	// so we use the index against the section, not the listView
-	var currentItem = $.listSection.getItemAt(_event.itemIndex);
+    // get data using index provided, the items are in the section
+    // so we use the index against the section, not the listView
+    var currentItem = $.listSection.getItemAt(_event.itemIndex);
 
-	// we save the data as a property so that is how we access
-	// the data, we do not need the data array in this case
-	var sessionInfo = currentItem.properties.data;
+    // we save the data as a property so that is how we access
+    // the data, we do not need the data array in this case
+    var sessionInfo = currentItem.properties.data;
 
-	// now display the data
-	alert("clicked on " + JSON.stringify(sessionInfo, null, 2));
+    // now display the data
+    alert("clicked on " + JSON.stringify(sessionInfo, null, 2));
 }
 
 // lets get some real data!!
@@ -66,96 +67,136 @@ Alloy.Globals.parseService.init();
 var parseService = Alloy.Globals.parseService;
 
 function saveSessionClicked() {
-	var queryResults;
+    var queryResults;
 
-	alert("WIP");
-	return;
+    alert("WIP");
+    return;
 
-	parseService.createObject('TutorSession', {
-		sessionName : $.sessionName.value,
-		sessionLocation : $.sessionLocation.value
-	}).then(function(_queryResult) {
-		console.log("Success Happened: " + JSON.stringify(_queryResult));
-		queryResults = _queryResult;
-		return parseService.getObjects('TutorSession');
-	}).then(function(_sessions) {
-		updateList(_sessions.response.results);
-	}, function(_error) {
-		console.log("Some Error Happened: " + JSON.stringify(_error));
-	});
+    parseService.createObject('TutorSession', {
+        sessionName : $.sessionName.value,
+        sessionLocation : $.sessionLocation.value
+    }).then(function(_queryResult) {
+        console.log("Success Happened: " + JSON.stringify(_queryResult));
+        queryResults = _queryResult;
+        return parseService.getObjects('TutorSession');
+    }).then(function(_sessions) {
+        updateList(_sessions.response.results);
+    }, function(_error) {
+        console.log("Some Error Happened: " + JSON.stringify(_error));
+    });
 }
 
 function updateList(_data) {
-	var items = _.map(_data, function(element) {
+    var items = _.map(_data, function(element) {
 
-		return {
-			properties : {
-				data : element // save all attributes
-			},
-			// bind the labels using the bindId
-			name : {
-				text : element.tutor.email
-			},
-			location : {
-				text : element.place.Location + "-" + element.place.Name
-			}
-		};
-	});
-	// add the items to the section in the ListView
-	$.listSection.setItems(items);
+        return {
+            properties : {
+                data : element // save all attributes
+            },
+            // bind the labels using the bindId
+            name : {
+                text : element.tutor.email
+            },
+            location : {
+                text : element.place.Location + "-" + element.place.Name
+            }
+        };
+    });
+    // add the items to the section in the ListView
+    $.listSection.setItems(items);
 
 }
 
 function whereQueryExample() {
 
-	// find all tutoring sessions that are being done by
-	// the tutors with the specified ids
-	var whereQueryStr = {
-		"tutor" : {
-			"$inQuery" : {
-				"where" : {
-					"objectId" : {
-						"$in" : ["SCV3V0GqRr", "9uKbg0Hzeb"]
-					}
-				},
-				"className" : "_User"
-			}
-		}
-	};
+    // find all tutoring sessions that are being done by
+    // the tutors with the specified ids
+    var whereQueryStr = {
+        "tutor" : {
+            "$inQuery" : {
+                "where" : {
+                    "objectId" : {
+                        "$in" : ["SCV3V0GqRr", "9uKbg0Hzeb"]
+                    }
+                },
+                "className" : "_User"
+            }
+        }
+    };
 
-	return parseService.getObjects('TutorSession', {
-		"urlparams" : {
-			"include" : "user,tutor,place",
-			"where" : whereQueryStr
-		}
-	});
+    return parseService.getObjects('TutorSession', {
+        "urlparams" : {
+            "include" : "user,tutor,place",
+            "where" : whereQueryStr
+        }
+    });
 }
+
+//
+// check to see if we have a user account saved
+//
 
 /**
  * logging in a user and then testing some of the functionality
  * of the rest service
  */
-parseService.loginUser("aaronsaunders", "password").then(function(_result) {
-	console.log("Success: " + JSON.stringify(_result, null, 2));
-	//return parseService.getObjects('TutorSession');
 
-	return whereQueryExample();
+function doLoginSuccess() {
+    return whereQueryExample().then(function(_sessions) {
+        console.log("getTutorSessions: " + JSON.stringify(_sessions.response, null, 2));
 
-}).then(function(_sessions) {
-	console.log("getTutorSessions: " + JSON.stringify(_sessions.response, null, 2));
+        updateList(_sessions.response.results);
 
-	updateList(_sessions.response.results);
+        // open the main view of index.js, which is the tab
+        $.index.open();
 
+    }, function(_error) {
+        Ti.API.error('ERROR: ' + JSON.stringify(_error, null, 2));
+        alert("Error Loading Data\n" + _error.error);
+
+    });
+}
+
+function loginUser() {
+    return parseService.loginUser("aaronsaunders", "password").then(function(_result) {
+        console.log("logged in user successfully: " + JSON.stringify(_result, null, 2));
+        return doLoginSuccess();
+    }, function(_error) {
+        Ti.API.error('ERROR: ' + JSON.stringify(_error, null, 2));
+        alert("Check Your Username/Password\n" +  _error.error.error);
+
+        //  loginUser();
+    });
+}
+
+
+parseService.restoreUser().then(function(_result) {
+    console.log("restored user successfully: " + JSON.stringify(_result, null, 2));
+    return doLoginSuccess();
 }, function(_error) {
-	Ti.API.error('ERROR: ' + JSON.stringify(_error, null, 2));
-	alert("Check Your Username/Password\n" + _error.error);
+    Ti.API.error('ERROR: ' + JSON.stringify(_error, null, 2));
+    console.log("no  user found" + _error.error);
+
+    return loginUser();
 });
 
+// Logout Button Handler
+$.logoutBtn.addEventListener('click', doLogout);
+
+function doLogout() {
+    parseService.logoutUser().then(function(_result) {
+        console.log("logged out user successfully: " + JSON.stringify(_result, null, 2));
+    }, function(_error) {
+        Ti.API.error('ERROR: ' + _error.error);
+        alert("Error Logging Out User\n" + _error.error.error);
+    });
+}
+
+//
+// PLACE HOLDER PUSH NOTIFICATION CODE
+//
 Ti.App.addEventListener("parse.push.recieved", function(_event) {
-	//if clicks push tray, it will take to alert list page
+    //if clicks push tray, it will take to alert list page
 
-	OS_IOS && Titanium.UI.iPhone.setAppBadge(0);
+    OS_IOS && Titanium.UI.iPhone.setAppBadge(0);
 });
-
-// open the main view of index.js, which is the tab
-$.index.open();
